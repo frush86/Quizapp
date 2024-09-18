91b7883563bcd8e767ad06c27d1fcdd92184b39c
Leonard <leonard.momber@posteo.de>
2024-09-18 11:57:50 +0200

    show hide java

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 const bodyElement = document.querySelector('[data-js="body"]');
 
 const darkmodebutton = document.querySelector('[data-js="dark-mode-button"]');
@@ -9,4 +10,33 @@ darkmodebutton.addEventListener("click", () => {
 
 darkmodetoggle.addEventListener("click", () => {
   bodyElement.classList.toggle("dark");
+});
+*/
+
+// CHANGE BOOKMARK
+
+const bookmark = document.querySelector('[data-js="bookmark"]');
+
+bookmark.addEventListener("click", () => {
+  if (bookmark.innerHTML === "🏳️") {
+    bookmark.innerHTML = "🚩";
+  } else {
+    bookmark.innerHTML = "🏳️";
+  }
+});
+
+// SHOW HIDE ANSWER
+
+const showanswer = document.querySelector('[data-js="showanswer"]');
+const answer = document.querySelector('[data-js="answer"]');
+
+showanswer.addEventListener("click", () => {
+  console.log("show pls");
+  if (answer.style.visibility === "hidden") {
+    answer.style.visibility = "visible";
+    showanswer.textContent = "Hide Answer";
+  } else {
+    answer.style.visibility = "hidden";
+    showanswer.textContent = "Show Answer";
+  }
 });
